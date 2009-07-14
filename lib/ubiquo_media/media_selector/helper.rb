676f53192115d70c8cc6d308fce7ba7802025590
Albert Callarisa <shark234@gmail.com>
2009-07-14 13:00:32 +0200

    Added forced visibility [#143 state:resolved]

--- lib/ubiquo_media/media_selector/helper.rb
+++ lib/ubiquo_media/media_selector/helper.rb
@@ -16,7 +16,7 @@ module UbiquoMedia
           :field => field,
           :object => form.object,
           :object_name =>  options[:object_name] || form.object_name.to_s,
-          :visibility => options[:visibility],
+          :visibility => Ubiquo::Config.context(:ubiquo_media).get(:force_visibility) || options[:visibility],
           :counter => @counter
         }
         render :partial => 'ubiquo/asset_relations/media_selector.html.erb', :locals => locals
