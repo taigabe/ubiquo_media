8261594a335776db11048a2214d907be0852d70e
Bernat Foj Capell <bfoj@gnuine.com>
2010-10-19 17:24:50 +0200

    Camelize connectors name instead of classifying them [#360]

--- lib/ubiquo_media/connectors.rb
+++ lib/ubiquo_media/connectors.rb
@@ -4,7 +4,7 @@ module UbiquoMedia
     autoload :Standard, "ubiquo_media/connectors/standard"
     
     def self.load!
-      "UbiquoMedia::Connectors::#{Ubiquo::Config.context(:ubiquo_media).get(:connector).to_s.classify}".constantize.load!
+      "UbiquoMedia::Connectors::#{Ubiquo::Config.context(:ubiquo_media).get(:connector).to_s.camelize}".constantize.load!
     end
   end
 end
