7a06a040d684dfe90947ca779299429748380688
David Gómez <david.gomez@marsbased.com>
2016-05-18 11:41:24 +0200

    Ubiquo Upgrade

--- app/models/asset_public.rb
+++ app/models/asset_public.rb
@@ -1,12 +1,3 @@
 class AssetPublic < Asset
-  file_attachment :resource,
-                  :visibility => "public",
-                  :styles     => self.correct_styles(Ubiquo::Config.context(:ubiquo_media).get(:media_styles_list)),
-                  :processors => Ubiquo::Config.context(:ubiquo_media).get(:media_processors_list),
-                  :storage    => Ubiquo::Config.context(:ubiquo_media).get(:media_storage)
-
-  validates_attachment_presence :resource
-
-  before_post_process :clean_tmp_files
-  after_resource_post_process :generate_geometries
+  include UbiquoMedia::Concerns::Models::AssetPublic
 end
---
+++ lib/ubiquo_media/concerns/models/asset_public.rb
@@ -0,0 +1,19 @@
+module UbiquoMedia::Concerns::Models::AssetPublic
+  extend ActiveSupport::Concern
+
+  included do
+    file_attachment :resource,
+                    :visibility => "public",
+                    :styles     => self.correct_styles(Ubiquo::Config.context(:ubiquo_media).get(:media_styles_list)),
+                    :processors => Ubiquo::Config.context(:ubiquo_media).get(:media_processors_list),
+                    :storage    => Ubiquo::Config.context(:ubiquo_media).get(:media_storage)
+
+    validates_attachment_presence :resource
+
+    before_post_process :clean_tmp_files
+    after_resource_post_process :generate_geometries
+  end
+
+  module ClassMethods
+  end
+end
