7b1fae324ecf82160be5b584aa9698a231c4c41b
Francisco Lopez <fjsanpedro@gmail.com>
2016-11-10 08:59:07 +0100

    Removed validation

--- app/models/asset_relation.rb
+++ app/models/asset_relation.rb
@@ -3,6 +3,7 @@ class AssetRelation < ActiveRecord::Base
   belongs_to :related_object, :polymorphic => true
 
   validates_presence_of :asset
+
   # Do not add
   # validates_length_of :name, :maximum => 255
 
